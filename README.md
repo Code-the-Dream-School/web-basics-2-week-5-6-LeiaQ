3541a6e7f0d1d842bfabd6c6eef7ef587ac5b620
Mariandrea Maldonado <mariandrea.mv15@gmail.com>
2020-05-22 15:32:41 -0400

    adding battleship challenge game using the DOM

---
+++ README.md
@@ -0,0 +1,6 @@
+# Web Basics 2 Week 5
+Follow the directions in the  [pdf](https://github.com/Code-the-Dream-School/web-basics-2-week-5/blob/master/BattleshipDOM.pdf) file or in your index.js file to do the assignment.
+
+Create issues on the link below on the parent repo if you are stuck and want to talk about it. You can paste code, tag people and refer to other issues there. Please use it! It will be helpful for mentors and future students to see where common problems are. Link here: https://github.com/Code-the-Dream-School/web-basics-2-week-5/issues
+
+In order to open the assignment open the index.html file inside of browser of your choosing. If you are using VSCode then you can install the OpenInBroswer extension, right click on index.html from VSCode and you will have an option at the bottom to open in your default browser or a browser of your choosing.
---
+++ index.js
@@ -0,0 +1,38 @@
+
+//------------------------ Game Project---------------------------
+//Do you remember the game Battleship we created before? well .... it is time to make it with the DOM!!
+//We are providing you with the design of a board (in the DOM) for a player1, you have to create the board for the player2 using the id property 'board_player2' -> it is the second list (ul) in your index.html file
+//First ask the players for their names (use propmt)
+//Now each time the turn player clicks on any cell of the opponent's board (you have to verify if the player is clicking the right board) the program needs to verify if there is an opponent's ship in that cell. If it is then the opponent has one less ship
+//We want you to store the data of each player in two Player objects. Each object has to store: name, remaining boats, and their respective board.
+//Each board needs to be initialized randomly with '0' and four '1' wich means the state of the cell. Numbers 1 are representing the 4 positions of the player's ships
+//Also we want you to display the name of the turn player in the tag that has the id 'turn_player'. And if there is a winner  a text with: 'Congratulationes {name_player}!! you win'
+//in the index.html file you are going to find 4 more ids: 'name_player1' , 'name_player2' , 'ships_player1' , 'ships_player2'. We want to see the information of each player in the respective elements
+//As our previous Battleship, the winner is the player that hits the 4 opponent's ships first
+//one more Thing create a 'reset' and a 'new game' buttons as childs of the element with the id 'buttons'. the reset button has to start the game again and the new game create a new game with new players and a new random board.
+
+const board_Player1 = document.getElementById('board_player1');
+
+for (var x = 0; x < 4; x++) {
+
+    const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
+
+    for (var y = 0; y < 4; y++) {
+      const cell = document.createElement('div');
+      cell.className = "square"; // adding css properties to make it looks like a square
+      cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
+      cell.value = 0;//state of the cell
+
+      //this function adds the click event to each cell
+      cell.addEventListener( 'click', (e) => {
+          let cell = e.target; // get the element clicked
+          console.log( cell.textContent) //display the coordinates in the console
+          cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
+          //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
+      });
+
+      li.appendChild(cell); //adding each cell into the row number x
+    }
+
+     board_Player1.appendChild(li); //adding each row into the board
+}
